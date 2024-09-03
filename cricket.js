5514161d22137c77563c6c356a474170dee51fcc
Basudev <samantabasu.2001@gmail.com>
2024-09-03 10:56:14 +0530

    Score Obeject Added

--- cricket.js
+++ cricket.js
@@ -1,3 +1,8 @@
+let score = {
+    Win: 0,
+    Lost: 0,
+    Tie: 0,
+};
 function computerGenerateChoice(){
     
     let randomNumber = Math.random() * 3; // This function generate random number  between 0 to 3
@@ -14,32 +19,46 @@ function computerGenerateChoice(){
 function getResultMsg(userMove, computerMove){
     if(userMove === 'bat'){
         if(computerMove === 'bat'){
+            score.Tie++;
             return `It's a tie`;
         }else if(computerMove === 'ball'){
+            score.Win++;
             return 'User has won';
         }else{
+            score.Lost++;
             return'Computer has won';
         }
     }else if(userMove === 'ball'){
         if(computerMove === 'bat'){
+            score.Lost++;
             return 'Computer has won';
         }else if(computerMove === 'ball'){
+            score.Tie++;
             return `It's a tie`;
         }else{
+            score.Win++;
             return 'User has won';
         }
     }else{
         if(computerMove === 'bat'){
+            score.Win++;
             return 'User has won';  
         }else if(computerMove === 'ball'){
+            score.Lost++;
             return 'Computer has won';
         }else{
+            score.Tie++;
             return `It's a tie`;
         }
     }
 }
 
 function showResult(userMove, computerMove,result){
+    // console.log(`Win: ${score.Win}, Lost: ${score.Lost}, Tie: ${score.Tie}`);
     
-    alert(`User chosen ${userMove}. Computer chosen ${computerMove} -> ${result}`);
+    alert(`User chosen ${userMove}. Computer chosen ${computerMove}
+    
+        ${result}
+    
+        Win: ${score.Win}, Lost: ${score.Lost}, Tie: ${score.Tie}`);
 }
